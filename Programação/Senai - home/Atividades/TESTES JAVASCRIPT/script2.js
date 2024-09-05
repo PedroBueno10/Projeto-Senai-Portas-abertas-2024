37b3c6d262f2df63ea7531d934dfcf1e5542f0f9
PedroBueno10 <136291339+PedroBueno10@users.noreply.github.com>
2024-09-05 15:45:35 -0300

    Delete Programação/Senai - home directory

--- Programação/Senai - home/Atividades/TESTES JAVASCRIPT/script2.js
+++
@@ -1,9 +0,0 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
-
-numeros.forEach(numero => {
-    if (numero % 2 === 0) {
-        console.log(numero + " é par");
-    } else {
-        console.log(numero + " é ímpar");
-    }
-});
